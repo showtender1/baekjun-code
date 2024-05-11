1acdded7e58eb5ab8111881c7b60c9b523c40f1a
showtender1 <109408296+showtender1@users.noreply.github.com>
2024-05-11 19:35:57 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -7,36 +7,37 @@ int operators[4];
 int minnum = 1000000001;
 int maxnum = -1000000001;
 void calculate(int res, int idx){
-    if(idx == n){
-        if(res > maxnum)
-            maxnum = res;
-        if(res < minnum)
-            minnum = res;
-        return;
-    }
-    for(int i = 0; i < 4; i++){
-        if(operators[i] > 0){
-            operators[i]--;
-            if(i == 0)
-                calculate(res + sequence[idx], idx+1);
-            else if(i == 1)
-                calculate(res - sequence[idx], idx+1);
-            else if(i == 2)
-                calculate(res * sequence[idx], idx+1);
-            else
-                calculate(res / sequence[idx], idx+1);
-            operators[i]++;
-        }
-    }
-    return;
+if(idx == n){
+if(res > maxnum)
+maxnum = res;
+if(res < minnum)
+minnum = res;
+return;
 }
+for(int i = 0; i < 4; i++){
+if(operators[i] > 0){
+operators[i]--;
+if(i == 0)
+calculate(res + sequence[idx], idx+1);
+else if(i == 1)
+calculate(res - sequence[idx], idx+1);
+else if(i == 2)
+calculate(res * sequence[idx], idx+1);
+else
+calculate(res / sequence[idx], idx+1);
+operators[i]++;
+}
+}
+return;
+}
+
 int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++)
-        cin >> sequence[i];
-    for(int i = 0; i < 4; i++)
-        cin >> operators[i];
-    calculate(sequence[0],1);
-    cout << maxnum << '\n';
-    cout << minnum;
+cin >> n;
+for(int i = 0; i < n; i++)
+cin >> sequence[i];
+for(int i = 0; i < 4; i++)
+cin >> operators[i];
+calculate(sequence[0],1);
+cout << maxnum << '\n';
+cout << minnum;
 }
