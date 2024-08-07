3687d2aeb7b9c64421fbeb5f4a7902b0f8c0d4c2
showtender1 <109408296+showtender1@users.noreply.github.com>
2024-08-07 21:46:10 +0900

    [Gold V] Title: 최소비용 구하기, Time: 36 ms, Memory: 5968 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <vector>
+#include <bits/stdc++.h>
+#include <queue>
+#define INF 987654321
+using namespace std;
+
+int arr[1005][1005];
+priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > >pq;
+int value[1005];
+
+int main(){
+	int n,m;
+	int u,v,w;
+	int start,end;
+	
+	scanf("%d %d",&n,&m);
+	
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			arr[i][j] = INF;
+	
+	for(int i=1;i<=n;i++){
+		arr[i][i] = 0;
+		value[i] = INF;
+	}
+	
+	for(int i=0;i<m;i++){
+		scanf("%d %d %d",&u,&v,&w);
+		if(arr[u][v]>w)
+		arr[u][v] = w;
+	}
+	
+	scanf("%d %d",&start,&end);
+	
+	value[start] = 0;
+	
+	pq.push(make_pair(0,start));
+	
+	while(!pq.empty()){
+		int x = pq.top().first;
+		int U = pq.top().second;
+		pq.pop();
+		
+		for(int i=1;i<=n;i++){			
+			int V = i;
+			int W = arr[U][i];
+			
+			if(W==INF) continue;
+			
+			if(x+W<value[V]){
+				value[V] = x + W;
+				pq.push(make_pair(x + W,V));
+			}
+		}
+	}
+	printf("%d",value[end]);
+	return 0;
+}
