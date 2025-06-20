57c60e13f28b6b75726e634afc098dffadf99461
showtender1 <109408296+showtender1@users.noreply.github.com>
2025-06-20 23:15:07 +0900

    [Silver IV] Title: 베스트셀러, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,17 @@
+d = dict()
+
+for _ in range(int(input())):
+    book = input()
+    if book in d:
+        d[book] += 1
+    else:
+        d[book] = 1
+
+max = max(d.values())
+ans = []
+for k, v in d.items():
+    if v == max:
+        ans.append(k)
+
+ans.sort()
+print(ans[0])
